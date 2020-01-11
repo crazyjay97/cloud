cdcea15081ade5df7ba73518435b714ef508ddfb
Professor X <zhanwj@zhiyuaniot.com>
2020-01-11 14:49:01 +0800

    init

--- feignclient/src/main/java/com/github/crazyjay97/feignclient/FeignclientApplication.java
+++ feignclient/src/main/java/com/github/crazyjay97/feignclient/FeignclientApplication.java
@@ -2,12 +2,14 @@ package com.github.crazyjay97.feignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker //熔断器
 public class FeignclientApplication {
 
     public static void main(String[] args) {
--- feignclient/src/main/java/com/github/crazyjay97/feignclient/controller/Controller.java
+++ feignclient/src/main/java/com/github/crazyjay97/feignclient/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.github.crazyjay97.feignclient.controller;
 
 import com.github.crazyjay97.feignclient.service.ApiService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,12 @@ public class Controller {
     private ApiService apiService;
 
     @RequestMapping("hello")
+    @HystrixCommand(fallbackMethod ="helloFallback")
     public String hello() {
         return "hello " + apiService.hello();
+    }
+
+    public String helloFallback() {
+        return "error";
     }
 }
